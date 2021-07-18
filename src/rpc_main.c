975bca9acbff31de1b55ede3534172372fd124a4
Dongwook Lee <elight.lee@gmail.com>
2021-07-18 19:31:09 +0900

    Create thread to run the observers

--- src/rpc_main.c
+++ src/rpc_main.c
@@ -1,5 +1,8 @@
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 #include "rpc_item.h"
 #include "rpc_server.h"
 #include "rpc_client.h"
@@ -9,27 +12,64 @@
 void printResponse(t_rpc_item item) {
 	uint8 buf[RPC_DATA_BUF_SIZE];
 	rpcRspDataRead(item, buf);
-	printf("[%d]: %s", item, buf);
+	printf("\n[%d]: %s", item, buf);
+}
+
+void reservePrintResponse(t_rpc_item item) {
+	uint8 buf[RPC_DATA_BUF_SIZE];
+	uint8 idx = 0;
+	sint8 i = 0;
+	rpcRspDataRead(item, buf);
+	while(buf[idx] != '\0' && idx < RPC_DATA_BUF_SIZE) {
+		idx++;
+	}
+	
+	printf("\n[%d]: ", item);
+	for(i = (idx - 1); i >= 0; i--) {
+		printf("%c", buf[i]);
+	}
 }
 
 void writeData(t_rpc_item item) {
-	uint8 buf[] = "RPC IS RUNNING!!!\n";
+	uint8 buf[] = "RPC IS RUNNING!!!";
 	rpcRspDataWrite(item, buf, sizeof(buf));
 	rpcSetProcessStat(item, RPC_PROCESS_OK);
 }
 
-int main (void) {
-	
-	rpcRegisterService(RPC_GENERAL_ITEM_3, writeData);
-	rpcRequestService(RPC_GENERAL_ITEM_3, printResponse);
-	
-	rpcRegisterService(RPC_GENERAL_ITEM_5, writeData);
-	rpcRequestService(RPC_GENERAL_ITEM_5, printResponse);
-	
+void *runObserverThread(void *arg) {
+
 	while(1) {
 		rpcRunServerObserver();
 		rpcRunClientObserver();
 	}
 	
+	return NULL;
+}
+
+int main (void) {
+	pthread_t thread_t;
+	int status;
+	
+	if(pthread_create(&thread_t, NULL, runObserverThread, NULL) < 0) {
+		perror("Couldn't run observer!!!");
+		exit(0);
+	}
+	
+	rpcRegisterService(RPC_GENERAL_ITEM_3, writeData);
+	rpcRegisterService(RPC_GENERAL_ITEM_5, writeData);
+	rpcRegisterService(RPC_GENERAL_ITEM_7, writeData);
+	
+	rpcRequestService(RPC_GENERAL_ITEM_3, printResponse);	
+	usleep(10000);	
+	rpcRequestService(RPC_GENERAL_ITEM_5, printResponse);	
+	usleep(10000);	
+	rpcRequestService(RPC_GENERAL_ITEM_7, reservePrintResponse);	
+	usleep(10000);	
+	rpcRequestService(RPC_GENERAL_ITEM_5, reservePrintResponse);
+	usleep(10000);
+	rpcRequestService(RPC_GENERAL_ITEM_3, reservePrintResponse);
+	
+	pthread_join(thread_t, (void **)&status);
+
 	return 0;
 }
--- src/type.h
+++ src/type.h
@@ -5,6 +5,9 @@
 typedef unsigned char uint8;
 typedef unsigned short int uint16;
 typedef unsigned int uint32;
+typedef signed char sint8;
+typedef signed short int sint16;
+typedef signed int sint32;
 
 #ifndef NULLPTR
 #define NULLPTR ((void *)(0))
