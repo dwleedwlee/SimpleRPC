5bd86d622975b5be0a06228bff59f91de058a4e4
Dongwook Lee <elight.lee@gmail.com>
2022-09-09 20:29:59 +0900

    Merge with the branch

--- src/rpc_client.c
+++ src/rpc_client.c
@@ -1,3 +1,26 @@
+/*
+	The MIT License (MIT)
+	
+	Copyright (c) 2021 Dongwook Lee
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+	the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <windows.h>
 
@@ -20,6 +43,11 @@ void rpcInitClientInfo(void) {
 	for(i = RPC_GENERAL_ITEM_START; i < RPC_GENERAL_ITEM_MAX; i++) {
 		g_rpcClientInfo[i].stat = RPC_CLIENT_READY;
 		g_rpcClientInfo[i].fp = NULLPTR;
+		
+		/* linked array */
+		g_rpcClientInfo[i].prev = RPC_GENERAL_ITEM_START;
+		g_rpcClientInfo[i].next = RPC_GENERAL_ITEM_START;
+		
 		#ifdef FEATURE_DUAL_BUFFER
 		gDualBufInfo[i].active_buf_idx = FALSE;
 		gDualBufInfo[i].stat = DUAL_BUF_STAT_EMPTY;
@@ -27,10 +55,28 @@ void rpcInitClientInfo(void) {
 	}
 }
 
+static void linkedArrayInsertItem(t_rpc_item item) {
+	/* linked array - insert the item next to the head */
+	t_rpc_item next = g_rpcClientInfo[RPC_GENERAL_ITEM_START].next;
+	g_rpcClientInfo[RPC_GENERAL_ITEM_START].next = item;
+	g_rpcClientInfo[item].prev = RPC_GENERAL_ITEM_START;
+	g_rpcClientInfo[item].next = next;
+	g_rpcClientInfo[next].prev = item;
+	
+}
+
+static void linkedArrayDeleteItem(t_rpc_item item) {
+	/* linked array - delete the item */			
+	t_rpc_item prev = g_rpcClientInfo[item].prev;
+	t_rpc_item next = g_rpcClientInfo[item].next;
+	g_rpcClientInfo[prev].next = g_rpcClientInfo[item].next;
+	g_rpcClientInfo[next].prev = g_rpcClientInfo[item].prev;
+}
+
 void rpcRunClientObserver(void) {
 	t_rpc_item i;
-	for(i = RPC_GENERAL_ITEM_START; i < RPC_GENERAL_ITEM_MAX; i++) {
-		
+	
+	for(i = g_rpcClientInfo[RPC_GENERAL_ITEM_START].next; i != RPC_GENERAL_ITEM_START; i = g_rpcClientInfo[i].next) {
 		switch (g_rpcClientInfo[i].stat) {
 			
 			case RPC_CLIENT_READY:
@@ -50,6 +96,8 @@ void rpcRunClientObserver(void) {
 						rpcClientStatWrite(i, RPC_CLIENT_REQUEST);
 					
 						if(gDualBufInfo[i].stat == DUAL_BUF_STAT_SINGLE) {
+							linkedArrayDeleteItem(i);
+						
 							gDualBufInfo[i].stat = DUAL_BUF_STAT_EMPTY;
 						} else { /* DUAL_BUF_STAT_FULL */
 							gDualBufInfo[i].active_buf_idx = (gDualBufInfo[i].active_buf_idx + 1) % 2;
@@ -71,14 +119,17 @@ void rpcRunClientObserver(void) {
 					case RPC_SERVER_ACCEPT:
 						/* Checking Timeout */ 
 						break;
-					case RPC_SERVER_UNDEFINED:
+					case RPC_SERVER_REJECT:
 					case RPC_SERVER_ERROR:
 					case RPC_SERVER_FINISH:
 					{
 						t_fp_cli_callback fp = g_rpcClientInfo[i].fp;						
 						if(fp != NULLPTR) {
 							fp(i); /* Blocking Call */
-						}						
+						}
+					#ifndef FEATURE_DUAL_BUFFER
+						linkedArrayDeleteItem(i);						
+					#endif
 						g_rpcClientInfo[i].stat = RPC_CLIENT_READY;
 						rpcClientStatWrite(i, RPC_CLIENT_READY);
 					}
@@ -106,7 +157,10 @@ t_rpc_req_ret rpcRequestService(t_rpc_item item, t_fp_cli_callback fp, t_rpc_buf
 			
 			idx = gDualBufInfo[item].active_buf_idx;
 			gDualBufInfo[item].fp[idx] = fp;
-			gDualBufInfo[item].rpc_buf[idx] = *rpc_buf;			
+			gDualBufInfo[item].rpc_buf[idx] = *rpc_buf;	
+			
+			linkedArrayInsertItem(item);			
+			
 			break;
 		case DUAL_BUF_STAT_SINGLE:
 			gDualBufInfo[item].stat = DUAL_BUF_STAT_FULL;
@@ -143,6 +197,8 @@ t_rpc_req_ret rpcRequestService(t_rpc_item item, t_fp_cli_callback fp, t_rpc_buf
 			g_rpcClientInfo[item].fp = fp;
 			
 			rpcClientStatWrite(item, RPC_CLIENT_REQUEST);
+			
+			linkedArrayInsertItem(item);
 			
 			ret = RPC_REQUEST_OK;
 		} else {
--- src/rpc_client.h
+++ src/rpc_client.h
@@ -1,3 +1,25 @@
+/*
+	The MIT License (MIT)
+	
+	Copyright (c) 2021 Dongwook Lee
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+	the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #ifndef __RPC_CLIENT_H__
 #define __RPC_CLIENT_H__
@@ -22,6 +44,9 @@ typedef void (*t_fp_cli_callback)(t_rpc_item item);
 typedef struct {
 	t_rpc_client_stat stat;
 	t_fp_cli_callback fp;
+	/* linked array */
+	t_rpc_item prev;
+	t_rpc_item next;
 }t_rpc_client_info;
 
 #ifdef FEATURE_DUAL_BUFFER
